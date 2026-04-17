ad1b26abee1bb355cc2d0b8aa8331cceca037b1c
lucasvieira <lucasvieira@google.com>
2026-04-17 17:49:41 +0000

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,36 @@
+console.log("Client side javascript file is loaded!");
+
+// fetch("https://swapi.dev/api/people/1").then((response) => {
+//   response.json().then((data) => {
+//     console.log(data.name);
+//   });
+// });
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const location = search.value;
+  console.log(location);
+
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
+
+  fetch(
+    "https://3000-firebase-node-course-1774895503464.cluster-zhw3w37rxzgkutusbbhib6qhra.cloudworkstations.dev/weather?address=" +
+      location
+  ).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
+});
---
+++ src/app.js
@@ -0,0 +1,113 @@
+require('dotenv').config();
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode.js");
+const forecast = require("./utils/forecast.js");
+
+const app = express();
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Setup handlebards engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+// recebe a view (index.hbs) e renderiza no navegador
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Lucas",
+  });
+});
+
+// route para a página about
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Lucas",
+  });
+});
+
+// route para a página help
+app.get("/help", (req, res) => {
+  res.render("help", {
+    message: "New message being sent",
+    title: "Help",
+    name: "Lucas",
+  });
+});
+
+// route para a página weather
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address",
+    });
+  }
+
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({
+          error: error,
+        });
+      }
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({
+            error: error,
+          });
+        }
+        res.send({
+          forecast: forecastData,
+          location: location,
+          address: req.query.address,
+        });
+      });
+    }
+  );
+});
+
+// app.get("/products", (req, res) => {
+//   if (!req.query.search) {
+//     return res.send({
+//       error: "You must provide a search term",
+//     });
+//   }
+
+//   console.log(req.query.search);
+//   res.send({
+//     products: [],
+//   });
+// });
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Lucas",
+    errorMessage: "Help article not found",
+  });
+});
+
+// qualquer página que não tenha um route acaba caindo nessa regra
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Lucas",
+    errorMessage: "Page not found",
+  });
+});
+
+// iniciar o server
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
