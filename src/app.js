b2198d2d6f49b520bacec57ebe3ae068c1cb9222
ninovieira22 <ninovieira22@gmail.com>
2026-04-24 22:30:14 -0300

    new settings on prettier

--- src/app.js
+++ src/app.js
@@ -1,82 +1,78 @@
 require('dotenv').config();
-const path = require("path");
-const express = require("express");
-const hbs = require("hbs");
-const geocode = require("./utils/geocode.js");
-const forecast = require("./utils/forecast.js");
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const geocode = require('./utils/geocode.js');
+const forecast = require('./utils/forecast.js');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates/views");
-const partialsPath = path.join(__dirname, "../templates/partials");
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebards engine and views location
-app.set("view engine", "hbs");
-app.set("views", viewsPath);
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
 hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 // recebe a view (index.hbs) e renderiza no navegador
-app.get("", (req, res) => {
-  res.render("index", {
-    title: "Weather App",
-    name: "Lucas",
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Lucas',
   });
 });
 
 // route para a página about
-app.get("/about", (req, res) => {
-  res.render("about", {
-    title: "About me",
-    name: "Lucas",
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About me',
+    name: 'Lucas',
   });
 });
 
 // route para a página help
-app.get("/help", (req, res) => {
-  res.render("help", {
-    message: "New message being sent",
-    title: "Help",
-    name: "Lucas",
+app.get('/help', (req, res) => {
+  res.render('help', {
+    message: 'New message being sent',
+    title: 'Help',
+    name: 'Lucas',
   });
 });
 
 // route para a página weather
-app.get("/weather", (req, res) => {
+app.get('/weather', (req, res) => {
   if (!req.query.address) {
     return res.send({
-      error: "You must provide an address",
+      error: 'You must provide an address',
     });
   }
 
-  geocode(
-    req.query.address,
-    (error, { latitude, longitude, location } = {}) => {
+  geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({
+        error: error,
+      });
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
       if (error) {
         return res.send({
           error: error,
         });
       }
-      forecast(latitude, longitude, (error, forecastData) => {
-        if (error) {
-          return res.send({
-            error: error,
-          });
-        }
-        res.send({
-          forecast: forecastData,
-          location: location,
-          address: req.query.address,
-        });
+      res.send({
+        forecast: forecastData,
+        location: location,
+        address: req.query.address,
       });
-    }
-  );
+    });
+  });
 });
 
 // app.get("/products", (req, res) => {
@@ -92,24 +88,24 @@ app.get("/weather", (req, res) => {
 //   });
 // });
 
-app.get("/help/*", (req, res) => {
-  res.render("404", {
-    title: "404",
-    name: "Lucas",
-    errorMessage: "Help article not found",
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Lucas',
+    errorMessage: 'Help article not found',
   });
 });
 
 // qualquer página que não tenha um route acaba caindo nessa regra
-app.get("*", (req, res) => {
-  res.render("404", {
-    title: "404",
-    name: "Lucas",
-    errorMessage: "Page not found",
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Lucas',
+    errorMessage: 'Page not found',
   });
 });
 
 // iniciar o server
 app.listen(port, () => {
-  console.log("Server is running on port" + port);
+  console.log('Server is running on port' + port);
 });
